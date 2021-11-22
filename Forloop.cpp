54a493f9b2cff5461ea3b699cd03e33c9894ac8e
Catherine Kimani <catherine.wachu@strathmore.edu>
2021-11-23 00:36:02 +0300

    Add project files.

---
+++ Forloop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	int num ;
+	
+	for (num = 12; num <= 28; num++)
+	{
+		if (num % 2 == 0)
+
+			cout << num << endl;
+	}
+	
+	return 0;
+}
